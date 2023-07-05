9586f24da53aa683026eb5868e0cb655caf2c7f3
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-05 05:39:16 +0000

    5 Jul 2023, 11:09

--- src/app/api/todos/router.tsx
+++ src/app/api/todos/router.tsx
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export  async function GET(req: NextApiRequest, res: NextApiResponse) {
   const { endpoint } = req.query;
-
+    console.log("endpoint",endpoint)
     const response = await fetch(`https://641fef8182bea25f6df72478.mockapi.io/api/v1/${endpoint}`);
     const data = await response.json();
     
