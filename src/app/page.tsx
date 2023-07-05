4388a8a666c8403cf3a96b814492037ae4b032a5
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-05 05:18:14 +0000

    5 Jul 2023, 10:48

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,25 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
-  type ToDos ={
-    id:string,
-    content:string,
-    checked:boolean
-  }  
-  const todos:ToDos[]=[
-      {
-        checked:true,
-        content:"hello",
-        id:'1'
-      }
-    ]
+type ToDos = {
+  id: string;
+  content: string;
+  checked: boolean;
+};
+
+const todos: ToDos[] = [];
+
+for (let i = 1; i <= 12; i++) {
+  const todo: ToDos = {
+    checked: true,
+    content: `Todo ${i}`,
+    id: i.toString(),
+  };
+
+  todos.push(todo);
+}
+
   return (<>
           <header className='flex justify-between items-center'> 
             <h1 className='text-2xl'>Todos</h1>
