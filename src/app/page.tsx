adaf486c8bc9928af3a3198996fe49e2e94fc800
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-05 06:10:44 +0000

    5 Jul 2023, 11:40

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+
 import axios from 'axios';
 import Link from 'next/link';
 import ToDoCard from '../../components/ToDoCard';
@@ -11,26 +11,14 @@ type ToDos = {
 
 const todos: ToDos[] = (await axios.get('https://641fef8182bea25f6df72478.mockapi.io/api/v1/todos')).data||[]
 
- 
-const toggleImportance =(id:string)=>{
-   
-    const todo= todos.find( todo => todo.id === id)
-    
-    if(todo){
-      axios.patch('https://641fef8182bea25f6df72478.mockapi.io/api/v1/todos',{...todo,checked:!todo.checked})
-    }
-    else{
-      console.error("Not found")
-    }
-    
-}
+
   return (<>
           <header className='flex justify-between items-center'> 
             <h1 className='text-2xl'>Todos</h1>
             <Link href='/new'>New</Link>
              </header>
           <ul className='pl-4'>{
-          todos.map(t => <ToDoCard  key={t.id} {...t} toggle={toggleImportance} />)
+          todos.map(t => <ToDoCard   key={t.id} {...t} />)
          }  
          
          </ul>     
