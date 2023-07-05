c5b41a3549019e0b6a908a2c73aade00e40d9cdc
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-05 05:15:08 +0000

    5 Jul 2023, 10:45

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,18 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Home() {
+  type ToDos ={
+    id:string,
+    content:string,
+    checked:boolean
+  }  
+  const todos:ToDos[]=[
+      {
+        checked:true,
+        content:"hello",
+        id:'1'
+      }
+    ]
   return (<>
           <header className='flex justify-between items-center'> 
             <h1 className='text-2xl'>Todos</h1>
