541395b8e32dc952b40a896dd7c1acc9e4989bb8
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-05 06:14:08 +0000

    5 Jul 2023, 11:44

--- components/ToDoCard.tsx
+++ components/ToDoCard.tsx
@@ -1,12 +1,14 @@
-'use client'
+
 import React from 'react'
 
 type ToDoCardData={
             id:string,
             content:string,
             checked:boolean
-            toggle:(id:string)=>void
+
 }
+
+
 const ToDoCard = (data:ToDoCardData) => {
    
 
@@ -14,7 +16,7 @@ const ToDoCard = (data:ToDoCardData) => {
     <div key={data.id}> 
           <div>{data.content}</div> 
           <span>
-            <input onClick={()=>{data.toggle(data.id)}} type='checkbox' checked={data.checked}/>
+            <input  type='checkbox' checked={data.checked}/>
          </span>
          </div>
   )
